251da08b7aaf2be50e5fbd0ce3078eb383ecc1a5
Tao Wen <taowen@chengfayun.com>
2021-03-21 16:18:52 +0800

    model must be complete

--- index.ts
+++ index.ts
@@ -2,10 +2,11 @@
 // 没有直接使用 babel 的 AST 数据结构，是为了方便用户独立构造
 // 在提供最终用户编程性的时候，Model 不是来自于 TypeScript 定义，而是由用户通过表单在运行时定义的
 export interface Model<T = any> {
+    cacheHash: number;
     // 路径 + 类名，用 path.basename 取得类名部分
     qualifiedName: string;
-    // 如果定义了 class，则是有值，否则为空
-    tableName?: string;
+    // 类名
+    tableName: string;
     // 基类的名字
     archetype?: string;
     // class 上的 decorator
